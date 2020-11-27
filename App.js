7bf22651dc2cfb477b739fe12ed4d41cd99793e3
BushraSyed837 <bushraabbas368@gmail.com>
2020-11-27 22:49:18 +0500

    Lab task completed

--- App.js
+++ App.js
@@ -1,394 +1,186 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, Modal, View, Text, Button, TextInput } from 'react-native';
 
 export default class Numbergame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
-      rand: Math.floor(Math.random() * 10),
-      actual: 0,
-      turn: 0,
-      score: 0,
-      hint: '',
-      round: 0,
-      transition: 0,
-      hintTaken: 0,
-      disable:true,
+      finalPrice: 0.0,
+      save: 0,
+      discount: 0.0,
+      price: 0.0,
+      history: ["History\n"],
+      transition: 2,
+      value: 0,
     };
   }
-  handle = (a) => {
-    this.setState({ text: (this.state.text = a) });
-  };
-  guessNumber = () => {
-    if (this.state.turn < 5) {
-      if (this.state.rand == this.state.text) {
-        alert('correct');
-        this.setState({ turn: (this.state.turn = 0) });
-        this.setState({
-          score: (this.state.score = this.state.score + 10),
-          rand: Math.floor(Math.random() * 10),
-        });
-        this.setState({ actual: this.state.rand });
-      } else {
-        alert('Not correct');
-        this.setState({ score: (this.state.score = this.state.score + 0) });
-        this.setState({ turn: (this.state.turn += 1) });
-      }
-    } else {
-      alert('game stopped');
-      this.setState({ text: (this.state.text = [0]) });
-      this.setState({ turn: this.state.turn == 5 });
-      this.setState({ turn: this.state.disable=false });
-      
-    }
-    this.setState({ hint: (this.state.hint = '') });
-  };
-  hint = () => {
-    if (this.state.rand < 5) {
-      if (this.state.rand > this.state.text) {
-        this.setState({
-          hint: (this.state.hint = 'The number is between 0-5'),
-        });
-        alert('the number you are guessing is less then actual number ');
-      } else {
-        alert('the number you are guessing is greater then actual number ');
-      }
-    } else {
-      this.setState({ hint: (this.state.hint = 'The number is between 5-10') });
-      if (this.state.rand > this.state.text) {
-        alert('the number you are guessing is less then actual number ');
-      } else {
-        alert('the number you are guessing is greater then actual number ');
-      }
-    }
-    this.setState({ hintTaken: this.state.hintTaken + 1 });
-    this.setState({ score: this.state.score - 2 });
-  };
-  reset(b) {
-    if (b === 'reset') {
-      this.setState({ text: (this.state.text = 0) });
-      this.setState({ actual: (this.state.actual = 0) });
-      this.setState({ score: (this.state.score = 0) });
-      this.setState({ hint: (this.state.hint = '') });
-      this.setState({ turn: (this.state.turn = 0) });
-    } else if (b === 'bck') {
-      this.setState({
-        text: (this.state.text = 0),
-      });
-    }
+  setValues = (e) => {
+    this.setState({ finalPrice: 0 })
+    this.setState({ discount: this.state.discount = (this.state.discount / 100) })
+    this.setState({ finalPrice: this.state.finalPrice = (this.state.price - (this.state.discount * this.state.price)) })
+    this.setState({ save: this.state.save = (this.state.discount * this.state.price) })
+    this.setState({ finalPrice: (this.state.finalPrice -= (this.state.finalPrice % .01)) })
+    this.setState({ save: (this.state.save -= (this.state.save % .01)) })
+    this.setState({ history: this.state.history += ("Original Price " + this.state.price + "\n") })
+    this.setState({ history: this.state.history += (("You Pay: " + (this.state.finalPrice) + "\n")) })
+    this.setState({ history: this.state.history += ("You Save: " + this.state.save + "\n") })
+    alert(this.state.history)
   }
-  updateTransition = (z) => {
-    this.setState({ transition: this.state.transition + 1 });
-    if(z==1){
-      this.setState({ transition: this.state.transition = 0 });
-      this.setState({ text: (this.state.text = 0) });
-      this.setState({ actual: (this.state.actual = 0) });
-      this.setState({ score: (this.state.score = 0) });
-      this.setState({ hint: (this.state.hint = '') });
-      this.setState({ turn: (this.state.turn = 0) });
-    }
-
-  };
-  mainScreen = () => {
-    return (
-      <View style={{ backgroundColor: '#3A4655', flex: 1 }}>
-        <View>
-          <Text
-              style={{
-                width:"100%",
-                color: '#fff',
-                textAlign:"center",
-                fontSize:18,
-                marginTop: "8%",
-                marginLeft: "3%",
-                marginRight: "3%",
-                fontFamily: 'Arial',
-                fontWeight: 'bold',
-                marginLeft: '7%',
-              }}>
-              Score: {this.state.score}
-            </Text>
-          </View >  
-          <View style={{width:"33%",marginLeft: "7%"}}>
-          <Button
-              color="red"
-              title="Statistic"
-              disabled={this.state.disable}
-              onPress={this.updateTransition.bind()}
-          />  
-          </View>                 
-        </View>
-        <View>
-              <Text
-                style={{
-                  color: '#fff',
-                  fontFamily: 'Arial',
-                  fontWeight: 'bold',
-                  marginTop: '5%',
-                  marginLeft: '2%',
-                  textAlign: 'left',
-                }}>
-                Hint: {this.state.hint}
-              </Text>
-            </View>            
-              <Text
-                style={{
-                  color: '#fff',
-                  fontFamily: 'Arial',
-                  fontWeight: 'bold',
-                  marginTop: '5%',
-                  marginLeft: '2%',
-                  textAlign: 'left',
-                }}>
-                Actual number:{this.state.actual}
-              </Text>
-            <View>
-              <Text
-                style={{
-                  color: '#fff',
-                  fontFamily: 'Arial',
-                  fontWeight: 'bold',
-                  marginTop: '5%',
-                  marginLeft: '2%',
-                  textAlign: 'left',
-                }}>
-                Your guess: {this.state.text}
-              </Text>
+  display = () => {
+    if (this.state.transition == 1) {
+      return (
+        <View style={{ backgroundColor: '#3A4655', flex: 1 }}>
+          <Modal animationType="slide" transparent={true} visible={this.state.modal} onRequestClose={() => { this.setState({ transition: (this.state.transition = 2) }) }}>
+            <View style={styles.centeredView}>
+              <Text style={{ fontSize: 13, color: "black" }}>{this.state.history}</Text>
+              <View style={styles.btn}>
+                <View>
+                  <Button
+                    color="red"
+                    title={'close'}
+                    onPress={() => { this.setState({ transition: (this.state.transition = 2) }) }}
+                  />
+                </View>
+              </View>
             </View>
+          </Modal>
+        </View>
+      )
+    }
+    else if (this.state.transition == 2) {
+      return (
+        <View style={{ backgroundColor: '#3A4655', flex: 1 }}>
+          <View style={styles.heading}>
+            <Text style={{ color: "red", fontSize: 18, textAlign: "center" }}>Discount Calculator</Text>
           </View>
-          <View style={styles.calcButtonRow}>
-            <View style={{ width: '20%', marginTop: '5%' }}>
+          <View style={{ borderWidth: 1, marginLeft: "8%", marginRight: "8%", borderColor: '#fff', }}>
+            <View style={styles.text}>
+              <Text style={{ color: '#fff', fontSize: 17, marginBottom: "3%" }}>You Pay: {this.state.finalPrice}</Text>
+              <Text style={{ color: '#fff', fontSize: 17, marginBottom: "3%" }}>You Save: {this.state.save}</Text>
+            </View>
+            <View>
               <Button
-                color="#425062"
-                title="Hint"
-                onPress={this.hint.bind(this, 'hint')}
+                color="red"
+                title={"history"}
+                onPress={() => { this.setState({ transition: (this.state.transition = 1) }) }}
               />
             </View>
           </View>
-          <View style={{ marginTop: '5%' }}>
-            <View style={styles.calcButtonRow}>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="Reset"
-                  onPress={this.reset.bind(this, 'reset')}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="Guess"
-                  onPress={this.guessNumber.bind(this, 'guess')}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="Back"
-                  onPress={this.reset.bind(this, 'bck')}
-                />
-              </View>
-            </View>
-            <View style={styles.calcButtonRow}>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="1"
-                  onPress={this.handle.bind(this, 1)}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="2"
-                  onPress={this.handle.bind(this, 2)}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="3"
-                  onPress={this.handle.bind(this, 3)}
-                />
-              </View>
-            </View>
-            <View style={styles.calcButtonRow}>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="4"
-                  onPress={this.handle.bind(this, 4)}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="5"
-                  onPress={this.handle.bind(this, 5)}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="6"
-                  onPress={this.handle.bind(this, 6)}
-                />
-              </View>
-            </View>
-            <View style={styles.calcButtonRow}>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="7"
-                  onPress={this.handle.bind(this, 7)}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="8"
-                  onPress={this.handle.bind(this, 8)}
-                />
-              </View>
-              <View style={{ width: '33%' }}>
-                <Button
-                  color="#425062"
-                  title="9"
-                  onPress={this.handle.bind(this, 9)}
-                />
-              </View>
-            </View>
-            <View style={styles.calcButtonRow}>
-              <View style={{ width: '99%' }}>
-                <Button
-                  color="#425062"
-                  title="0"
-                  onPress={this.handle.bind(this, 0)}
-                />
-              </View>
-            </View>
+          <View>
+            <TextInput
+              style={styles.textInput}
+              color="#fff"
+              placeholder="Original Price"
+              keyboardType="number-pad"
+              onChangeText={(value) => {
+                this.setState({ price: (this.state.price = value) })
+                }                
+              }
+              placeholderTextColor="grey"
+            ></TextInput>
           </View>
-        </View>
-      );
-    } else if (this.state.transition == 2) {
-      return (        
-        <View style={{ backgroundColor: '#3A4655', flex: 1 }}>
-          <View style={styles.heading}>
-            <Text
-              style={{
-                color: 'red',
-                fontFamily: 'Arial',
-                fontWeight: 'bold',
-                textAlign: 'center',
-              }}>
-              GUESS A NUMBER
-            </Text>
+          <View>
+            <TextInput
+              style={styles.textInput}
+              color="#fff"
+              placeholder="Discount Price"
+              keyboardType="number-pad"
+              onChangeText={(value) => {
+                if (value <= 100) {
+                  this.setState({ discount: (this.state.discount = value) })
+                  
+                }
+                else {
+                  this.setState({ discount: (this.state.discount = 0) })
+                  alert("The range of discount is too much")
+                }
+              }
+
+              }
+              placeholderTextColor="grey"
+            ></TextInput>
           </View>
-          <View style={styles.endGame}>
-            <Text
-              style={{
-                color: 'red',
-                fontFamily: 'Arial',
-                fontWeight: 'bold',
-                textAlign: 'center',
-              }}>
-              Game Finished
-            </Text>
-          </View>
-          <View style={styles.startGame}>
-             <View style={styles.body}>
+          <View style={styles.btn}>
             <View>
-             <Text
-              style={{
-                  color: '#fff',
-                  fontFamily: 'Arial',
-                  fontWeight: 'bold',
-                  marginTop: '5%',
-                  marginLeft: '2%',
-                  textAlign: 'left',
-              }}>
-              Score: {this.state.score}
-            </Text>
-              <Text
-                style={{
-                  color: '#fff',
-                  fontFamily: 'Arial',
-                  fontWeight: 'bold',
-                  marginTop: '5%',
-                  marginLeft: '2%',
-                  textAlign: 'left',
-                }}>
-                you took hint: {this.state.hintTaken} times
-              </Text>
-            </View>            
-              <Text
-                style={{
-                  color: '#fff',
-                  fontFamily: 'Arial',
-                  fontWeight: 'bold',
-                  marginTop: '5%',
-                  marginLeft: '2%',
-                  textAlign: 'left',
-                }}>
-                Actual number: {this.state.rand}
-              </Text>
+              <Button
+                color="red"
+                disabled={this.state.bb}
+                title={'Calculate'}
+                onPress={this.setValues.bind()}
+              />
             </View>
-         
-          </View>
-          <View style={styles.finish}>
-          <View style={{ width: '40%' }}>
-                <Button
-                  color="red"
-                  title="Finish"
-               />
-              </View>
-              <View style={{ width: '43%' }}>
-                <Button
-                  color="red"
-              disabled={this.state.bb}
-              title={'Calculate'}
-              onPress={this.setValues.bind(this, 2)}
-            />
           </View>
         </View>
+      )
+    }
+  }
+  render() {
+    return (
+      <View style={{ backgroundColor: '#3A4655', flex: 1 }}>
+        {this.display()}
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   body: {
-    flex: 0.7,
+    flex: 0.2,
     borderWidth: 5,
-    borderColor: '#FAEBD7',
-    margin: '3%',
+    borderColor: '#fff',
+    margin: '5%',
     justifyContent: 'center',
+  },
+  btn: {
+    flexDirection: 'row',
+    width: '33%',
+    marginLeft: '19%',
+    marginRight: '15%',
+    alignSelf: 'center',
+    marginTop: '5%',
+  },
+  text: {
+    borderColor: '#fff',
+    borderWidth: 0,
+    marginTop: '3%',
+    marginBottom: '3%',
+  },
+  centeredView: {
+    height: "50%",
+    width: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "25%",
+    color: "black",
+    backgroundColor: "white"
+
+  },
+  textInput: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: 2,
+    marginTop: '3%',
+    flexDirection: 'row',
+    marginLeft: "10%",
+    marginRight: "10%",
+    justifyContent: "space-between",
+  },
+  calcBody: {
+    margin: 'auto',
+    minHeight: '400px',
   },
   calcButtonRow: {
     flexDirection: 'row',
     justifyContent: 'center',
     marginLeft: '10%',
     marginRight: '10%',
-    marginTop: '0%',
-  },
-  startGame: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginLeft: '10%',
-    marginRight: '10%',
-    marginTop: '35%',
-  },
-  finish:{
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginLeft: '10%',
-    marginRight: '10%',
   },
   heading: {
-    flex: 0.155,
+    flex: 0.29,
+    fontSize: 18,
+    color: 'red',
+    textAlign: 'center',
     justifyContent: 'center',
-    marginTop: '15%',
-    margin: '19%',
+    marginTop: '20%',
+    margin: '25%',
     borderWidth: 1,
     borderRadius: 0,
     borderColor: '#fff',
